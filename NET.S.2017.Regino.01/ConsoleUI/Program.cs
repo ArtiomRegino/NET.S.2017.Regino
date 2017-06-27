353872a0bb39feeb10f0694a3d3d3e01b1736bc1
Artiom Regino <artiom.regino@gmail.com>
2017-06-28 02:04:00 +0300

    Added first hw.

---
+++ NET.S.2017.Regino.01/ConsoleUI/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] testArrQSort = { 1, -2, 7, 4, 9, 5, -343, -12, 465, 9, 2, 45, -7, 9, -3 };
+
+            Console.WriteLine("Initial array: ");
+            foreach (var item in testArrQSort)
+            {
+                Console.WriteLine(item);
+            }
+
+            Sortings.QuickSort.Sort(testArrQSort, 0, testArrQSort.Length - 1);
+
+            Console.WriteLine("Sorted array: ");
+            foreach (var item in testArrQSort)
+            {
+                Console.WriteLine(item);
+            }
+
+            int[] testArrMSort = { 1, -2, 7, 4, 9, 5, -343, -12, 465, 9, 2, 45, -7, 9, -3 };
+
+            Console.WriteLine("Initial array: ");
+            foreach (var item in testArrMSort)
+            {
+                Console.WriteLine(item);
+            }
+
+            testArrMSort = Sortings.MergeSort.Sort(testArrMSort);
+
+            Console.WriteLine("Sorted array: ");
+
+            foreach (var item in testArrMSort)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ NET.S.2017.Regino.01/Sortings/QuickSort.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sortings
+{
+    public static class QuickSort
+    {
+        /// <summary>
+        /// Quick sort.
+        /// </summary>
+        /// <param name="array">Initial array.</param>
+        /// <param name="left">Left border of array(from where it will be sorted).</param>
+        /// <param name="right">Right border of array.</param>
+        public static void Sort(int[] array, int left, int right)
+        {
+            int pivot, leftend, rightend;
+
+            leftend = left;
+            rightend = right;
+            pivot = array[left];
+
+            while (left < right)
+            {
+                while ((array[right] >= pivot) && (left < right))
+                {
+                    right--;
+                }
+
+                if (left != right)
+                {
+                    array[left] = array[right];
+                    left++;
+                }
+
+                while ((array[left] <= pivot) && (left < right))
+                {
+                    left++;
+                }
+
+                if (left != right)
+                {
+                    array[right] = array[left];
+                    right--;
+                }
+            }
+
+            array[left] = pivot;
+            pivot = left;
+            left = leftend;
+            right = rightend;
+
+            if (left < pivot)
+            {
+                Sort(array, left, pivot - 1);
+            }
+
+            if (right > pivot)
+            {
+                Sort(array, pivot + 1, right);
+            }
+        }
+    }
+}
