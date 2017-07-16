bfafa5427fbe0cb33ede50872b70b68ab93a41b1
ArtiomRegino <artiom.regino@gmail.com>
2017-07-16 15:58:54 +0300

    8 hw

---
+++ NET.S.2017.Regino.08/CustomerFormat/Customer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerFormat
+{
+    public class Customer: IFormattable
+    {
+        
+        private string name;
+        private string contactPhone;
+        public decimal Revenue { get; set; }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (ReferenceEquals(value, null)) throw new ArgumentNullException();
+                name = value;
+            }
+        }
+
+        public string ContactPhone
+        {
+            get { return contactPhone; }
+            set
+            {
+                if (ReferenceEquals(value, null)) throw new ArgumentNullException();
+                contactPhone = value;
+            }
+        }
+
+        public Customer(string name, string phone, decimal revenue)
+        {
+            Name = name;
+            Revenue = revenue;
+            ContactPhone = phone;
+        }
+
+        public override string ToString()
+        {
+            return this.ToString("G", CultureInfo.CurrentCulture);
+        }
+
+        public string ToString(string format)
+        {
+            return this.ToString(format, CultureInfo.CurrentCulture);
+        }
+
+        public string ToString(string format, IFormatProvider provider)
+        {
+            if (string.IsNullOrEmpty(format)) format = "G";
+            if (provider == null) provider = CultureInfo.CurrentCulture;
+
+            switch (format.ToUpperInvariant())
+            {
+                case "G": return $"Customer: {Name}, Phone: {ContactPhone}, Revenue:{Revenue.ToString("C", provider)}";
+                case "K": return $"Customer: {Name},  Phone: {ContactPhone}";
+                case "B": return $"Customer: {Name}, Revenue:{Revenue.ToString("C", provider)}";
+                case "S": return $"Customer: {Name}";
+                case "R": return $"Customer: {Revenue.ToString("C", provider)}";
+                default: return "Customer.";
+            }
+        }
+
+        
+    }
+}
---
+++ NET.S.2017.Regino.08/CustomerFormat/CustomerProvider.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerFormat
+{
+    class CustomerProvider : IFormatProvider, ICustomFormatter
+    {
+
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+                return this;
+            else
+                return null;
+        }
+
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            if (ReferenceEquals(arg, null)) throw new ArgumentNullException();
+            if (arg.GetType() != typeof(Customer))
+                try {
+                    return HandleOtherFormats(format, arg);
+                }
+                catch (FormatException e) {
+                    throw new FormatException($"The format of '{format}' is invalid.", e);
+                }
+
+            format = format.ToUpper(CultureInfo.InvariantCulture);
+            Customer customer = arg as Customer;
+
+            if (!(format == "P" || format == "T"))
+                try
+                {
+                    return HandleOtherFormats(format, arg);
+                }
+                catch (FormatException e) {
+                    throw new FormatException($"The format of '{format}' is invalid.", e);
+                }
+
+            switch (format)
+            {
+                case "P":
+                    return $"Customer's phone:{customer.ContactPhone }";
+                case "T":
+                    return $"Requested data about customer {customer.Name}: Phone: {customer.ContactPhone}, Revenue:{customer.Revenue})";
+                default:
+                    return string.Empty;
+            }
+        }
+       
+        private string HandleOtherFormats(string format, object arg)
+        {
+            if (arg is IFormattable)
+                return ((IFormattable)arg).ToString(format, CultureInfo.CurrentCulture);
+            else if (arg != null)
+                return arg.ToString();
+            else
+                return String.Empty;
+        }
+    }
+}
