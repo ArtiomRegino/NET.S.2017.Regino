064db2170fa5f6de01176c48cf65960855257970
ArtiomRegino <artiom.regino@gmail.com>
2017-07-27 16:59:00 +0300

    -

---
+++ NET.S.2017.Regino.13/ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomQueue.Queue<int> queue = new CustomQueue.Queue<int>(3);
+
+            queue.Enqueue(1);
+            queue.Enqueue(1);
+            queue.Dequeue();
+            Console.WriteLine($"{queue.Begin}, {queue.End}");
+            queue.Enqueue(1);
+            Console.WriteLine(queue.Count);
+            Console.WriteLine($"{queue.Begin}, {queue.End}");
+            queue.Dequeue();
+            queue.Dequeue();
+            Console.WriteLine($"{queue.Begin}, {queue.End}");
+            queue.Enqueue(1);
+            queue.Enqueue(1);
+            Console.WriteLine($"{queue.Begin}, {queue.End}");
+            queue.Enqueue(1);
+            Console.WriteLine($"{queue.Begin}, {queue.End}");
+
+            Console.WriteLine(queue.Count);
+        }
+    }
+}
