95b76a62a50be62c4c56c45443f6d84273bdc64c
ArtiomRegino <artiom.regino@gmail.com>
2017-10-21 14:56:28 +0300

    -

--- JS/task_4/js/app.js
+++ JS/task_4/js/app.js
@@ -1,1 +1,47 @@
-var divLine = document.getElementById("divLine");
+window.addEventListener("load", function () {
+
+	var zombie;
+	var divLine = document.getElementById("divLine");
+	var btnGenerate = document.getElementById("Generate");
+	var btnMove = document.getElementById("Move");
+	var btnShot = document.getElementById("Shot");
+
+	btnGenerate.addEventListener("click", generate);
+	btnMove.addEventListener("click", move);
+	btnShot.addEventListener("click", shot);
+
+	function generate () {
+
+		if ( divLine.innerHTML == "" ) {
+
+			divLine.style.marginLeft = "530px";
+
+			zombie = new Zombie();
+			var zombieBlock = zombie.zombieBlock;
+
+			divLine.appendChild(zombieBlock);
+		}
+	}
+
+	function move () {
+		if (divLine.innerHTML != "") {
+			
+			var offset = zombie.move();
+			var previousMarginL = getComputedStyle(divLine).marginLeft; 
+
+			if ( parseInt(previousMarginL) >= offset ) {
+				var currentMarginL = parseInt(previousMarginL) - offset;
+
+				divLine.style.marginLeft = currentMarginL + "px";
+			}
+		}
+	}
+
+	function shot () {
+		if (divLine.innerHTML != "") {
+			zombie.hit(10);
+		}
+	}
+	
+})
+
--- JS/task_4/js/zombie.js
+++ JS/task_4/js/zombie.js
@@ -1,24 +1,49 @@
-function zombie () {
+function Zombie () {
 
 	var health = 100;
 
+	var zombieElement = document.createElement("div");
+	var healthDiv = document.createElement("div");
+	var gifDiv = document.createElement("div");
+	var killed = new CustomEvent("killed");
 
+	zombieElement.classList.add("zombieDiv");
+	gifDiv.classList.add("gifDiv");
+	healthDiv.classList.add("healthDiv");
+
+	zombieElement.appendChild(healthDiv);
+	zombieElement.appendChild(gifDiv);
+
+	on( "killed", function() {
+		zombieElement.parentNode.removeChild(zombieElement);
+	});
+
+	Object.defineProperty( this, "zombieBlock", {
+	 	get : function () {
+			return zombieElement;
+		}
+	});
 
 	function on ( eventName, eventCallback ) {
-
+		zombieElement.addEventListener(eventName, eventCallback);
 	}
 
-	function move () {
-		
+	this.move = function () {
+		return 10;
 	}
 
-	function hit ( damage ) {
+	this.hit = function ( damage ) {
 
-		if ( health >= damage ) {
+		if ( health > damage ) {
+			var healthWidth = getComputedStyle(healthDiv).width;
+			var newWidth = parseInt(healthWidth) - parseInt(healthWidth) / health * damage; 
+
 			health -= damage;
+			healthDiv.style.width = newWidth + "px";
+		} 
+		else {
+			zombieElement.dispatchEvent(killed);
 		}
-		
-
 	}
 
 }
